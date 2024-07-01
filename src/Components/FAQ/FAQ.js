bc992d496cd31b779b8458a22ba0442d2e92a459
aqadavinash <avinash@aqad.ae>
2024-07-02 01:39:28 +0530

    add

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -42,129 +42,79 @@ const Faq = ({ title }) => {
                 <li>
                   <div className="boldfont">How to delete user account ?</div>
                   <p>
-                    To request for user account and data deletion, please write
-                    to tech@Luxyara.in. We will revert with resolution within 48
-                    hours.
+                  To request for user account and data deletion, please write to hi@luxyaragroup.io We will revert with resolution within 48 hours.
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    I’m not sure about the size and fit of the frames.
+                    I’m sure about the size and fit of the your choice size.
                   </div>
                   <p>
-                    At Luxyara, we offer glasses in 3 sizes – small, medium, and
-                    large. Click the ‘Frame Size’ link at the bottom of the page
-                    to know your size. ~80-90% of our customers find a medium
-                    size frame to be the best fit.
+                    At Luxyara, we offer products in verious sizes –  Click the ‘Size Chart’ link at the bottom of the page to know your size. -100% of our customers find a Perfect size product to be the best fit.
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    What if I buy glasses & they don’t fit me? What if the
-                    lenses aren’t right ?
+                    What if I buy any product & they don’t fit me?
                   </div>
                   <p>
-                    Don’t worry! It is our responsibility to get the perfect
-                    pair of glasses. In the rare circumstance that your glasses
-                    don’t fit properly, we make it extremely easy for you to
-                    exchange or return the frames, with lenses. If you face any
-                    difficulty, just call us at 000000000 We have a No Questions
-                    Asked Return Policy - 14 Days for Eyeglasses & Contact
-                    Lenses and 7 Days for Sunglasses & Clips On. We provide
-                    instant money back or store credit on return.
+                  Don’t worry! It is our responsibility to get the perfect product . In the rare circumstance that your product don’t fit properly, we make it extremely easy for you to exchange or return the product . If you face any difficulty, just call us at +91, 8090522232 We have a No Questions Asked Return Policy - 14 Days for any our product . We provide instant money back or store credit on return.
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    Will the glasses have my prescription?
+                    Will the surgical belt have my prescription?
                   </div>
                   <p>
-                    You can buy glasses fitted with your prescription or even
-                    the frames standalone. We custom-make glasses with single
-                    vision, bifocal, or progressive prescriptions across all
-                    powers. Our glasses are ~50% cheaper than market average,
-                    since we bypass intermediaries – and best of all, we charge
-                    the same price, regardless of your power!
+                  You can buy your choice fitted with your prescription or even the a part of your requirement . We custom-make for your choice, or progressive prescriptions across all powers. Our Products are ~50% cheaper than market average, since we bypass intermediaries – and best of all, we charge the same price, regardless of your power!
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    Will Luxyara be able to make my complex power?
+                  Will Luxyara be able to make my complex requirement ?
                   </div>
                   <p>
-                    At Luxyara, we stock all powers – simple and complex. The
-                    unique robotic technology used in our lab ensures that the
-                    most complex powers are fitted perfectly, ensuring 100%
-                    error-free glasses. Our 10 lakh+ customers can vouch for the
-                    precision with which we make the eyeglasses!
+                  At Luxyara, we stock all powers – simple and complex. The unique robotic technology used in our lab ensures that the most complex powers are fitted perfectly, ensuring 100% error-free product. Our 10 lakh+ customers can vouch for the precision with which we make the thair product !
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    Where do I submit my eye power details?
+                  Where do I submit my choice requirement details?
                   </div>
                   <p>
-                    You can submit your power details after you complete your
-                    purchase. We offer multiple options to provide your
-                    prescription to us – upload a picture, enter it online,
-                    email us (power@Luxyara.com) or call us at 000000000
+                  You can submit your choice details after you complete your purchase. We offer multiple options to provide your choice to us – upload a picture, enter it online, email us (hi@luxyaragroup.io) or call us at +91, 8090522232 or Message us +91, 8090522232
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    I don’t have my prescription handy. Is it important to give
-                    it right away?
+                  I don’t have any choice handy. Is it important to give it right away?
                   </div>
                   <p>
-                    It’s ok if you don’t have your prescription handy while
-                    placing the order. Simply place the order for the frame and
-                    provide your power using the links provided in the SMS and
-                    email that we send you, post order completion. It’s that
-                    simple!
+                  It’s ok if you don’t have your choice handy while placing the order. Simply place the order for the as picture and provide your requirements using the links provided in the SMS and email that we send you, post order completion. It’s that simple!
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    I really, really want to try the frames on before making a
-                    purchase!
+                  I really, really want to try the feel on before making a purchase!
                   </div>
                   <p>
-                    Unsure? Use our 3D Try-on! Feature – click ‘3D Try-on!’ on
-                    the product page of your chosen frame, follow the
-                    instructions, and see a 180 degree view of yourself with
-                    your chosen frames! Share with your friends and family to
-                    find the perfect pair! In addition, we offer a unique ‘Free
-                    Home Trial’ service, across India. Select up to 5 frames
-                    (from our non-free frame categories) and our delivery
-                    executive will bring them to your doorstep for trial!Click
-                    here to try frames!
+                  Unsure? in future dear will Use our 3D Try-on! Feature – click ‘3D Try-on!’ on the product page of your chosenany product, follow the instructions, and see a 180 degree view of yourself with your chosen product! Share with your friends and family to find the perfect match! In addition, we offer a unique ‘Free Home Trial’ service, global. Select up to 5 products (from our non-free categories) and our delivery executive will bring them to your doorstep for trial!Click here to try products!
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    How can I be sure of the quality of the frames?
+                  How can I be sure of the quality of the Product?
                   </div>
                   <p>
-                    Quality is at the core of Luxyara’s products. Our plastic
-                    frames are made from the best quality acetate and our metal
-                    frames are made only from steel or titanium – this makes our
-                    glasses corrosion and breakage resistant. We also offer a 1
-                    year warranty on all our glasses – in case you have any
-                    issues, call us, and we’ll make sure it gets resolved!
+                  Quality is at the core of Luxyara’s products. Our any product are made from the best quality of genuine meterial and our leather Products are made only from natural leather as like buffallow / cow / Deerskin Hide / Crocodial / bull / Bison / Reindeer Skin  – this makes our products corrosion and breakage resistant. We also offer Lifetime warranty on all our any Product  – in case you have any issues, call us, and we’ll make sure it gets resolved!
                   </p>
                 </li>
                 <li>
                   <div className="boldfont">
-                    What lenses will you use? I use thin, anti-glare lenses.
+                  What type of leather will you use? I use Natutral Leather of  requirement thikness, anti-fungus.
                   </div>
                   <p>
-                    We understand that a pair of glasses is only as good as the
-                    lenses fitted in them. Most anti-glare lenses cost between
-                    Rs.1,500 - 2,000 at your local optician. At that price we
-                    give you robotically fitted, anti-glare lenses, with a 1.6
-                    index which is ~35% thinner than market average. In addition
-                    we offer UV-400 protection on all our lenses, similar to
-                    what is provided in lenses across Europe and the USA
+                  We understand that a product which is make by me good as the best quality . Most anti-fungle and anti bactrial product cost.  At this price we give you robotically fitted, anti-fungal Leather product , with a life time warrenty  than market average. In addition we offer 100% protection on all our Products, similar to what is provided in Products across USA and the Europe & Australia and Global Market.
                   </p>
                 </li>
                 <li>
