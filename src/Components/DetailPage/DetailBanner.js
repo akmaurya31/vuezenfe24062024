a31aa8bc09484da37ae036de7b9bc0b422060609
aqadavinash <avinash@aqad.ae>
2024-07-18 23:05:39 +0530

    add

--- src/Components/DetailPage/DetailBanner.js
+++ src/Components/DetailPage/DetailBanner.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import lensesImg from '../../Images/lenses.webp'
-
+//fasdf
 const DetailBanner = () => {
   return (
     <>
         <Container>
             <Row className='detailbanner-main'>
                 <Col md={6}>
-                    <h2>Light Frames for best comfort</h2>
-                    <h3>Feel the freedom of light weight frames</h3>
+                    <h2>Genuine Quality for best comfort</h2>
+                   
+                    <h3>Feel the freedom of light your choice</h3>
                 </Col>
                 <Col md={6} className='only-desktop'><img src={lensesImg}/></Col>
             </Row>
